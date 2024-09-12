7fc58b1f00fed4f3589641289fe5fa829f067be1
ocean connecting <zaaranaouali@gmail.com>
2024-09-12 15:37:06 +0100

    dssd

--- src/components/dashboard/overview/latest-orders.jsx
+++ src/components/dashboard/overview/latest-orders.jsx
@@ -30,7 +30,7 @@ export function LatestOrders({ sx }) {
   if (!order || order.length === 0) {
     return <p>No orders available.</p>; // Display a message if no orders are found
   }
-
+console.log("ewewdss")
   return (
     <Card sx={sx}>
       <CardHeader title="Latest orders" />
